4ec05a8e5df236006d2475c04aee78b2781746fb
otto <otto@otto-PC>
2010-03-29 23:26:29 +0200

    s

--- src/kth/projects/uppgift8/Path.java
+++ src/kth/projects/uppgift8/Path.java
@@ -3,16 +3,41 @@ package kth.projects.uppgift8;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import kth.projects.uppgift7.MatrixGraph;
 
 public class Path {
+	private static MatrixGraph graph;
+
 	private static void readFile(String fileName) {
+		String line;
 		try {
 			File file = new File(fileName);
 			Scanner scanner = new Scanner(file);
-			while (scanner.hasNext()) {
-				if(scanner.next().equals("//"))
-					System.out.println(scanner.next());
+			while(scanner.hasNext()) {
+				line = scanner.nextLine();
+				if(line.startsWith("//"))
+					System.out.print("");
+				else if(line.contains("// Size")) {
+					String[] sizes;
+					int size;
+					sizes = line.split(" ", 2);
+					System.out.println(sizes[0] + "Size");
+					
+					size = Integer.parseInt(sizes[0]);
+					graph = new MatrixGraph(size);
+				}
+				else if(line.length() != 0 && !line.startsWith("//")) {
+					int v, w, c;
+					String[] vertices;
+					vertices = line.split(" ", 6);
+					System.out.println(vertices[0] + " " + vertices[1] + " " + vertices[2]);
+					v = Integer.parseInt(vertices[0]);
+					w = Integer.parseInt(vertices[1]);
+					c = Integer.parseInt(vertices[2]);
+					graph.addEdge(v, w, c);
+				}
+				graph.toString();
 			}
 			scanner.close();
 		} catch (FileNotFoundException e) {
